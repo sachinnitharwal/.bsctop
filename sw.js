ba48a503361d6355374ecc2cc3d4f4d8329a0122
sachin nitharwal <sc8949352506@gmail.com>
2026-01-03 22:19:12 +0530

    update

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'bsctop-v2';
+const assets = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  '/logo.png',
+  '/about.html',
+  '/privacy.html',
+  '/terms.html'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
+});
